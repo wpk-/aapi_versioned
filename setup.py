567319b9e5c82c9af2d9ac606ee14d5f53347450
Paul <github@paulkoppen.com>
2022-04-26 22:41:18 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aapi_versioned',
+    version='1.1',
+    packages=['aapi_versioned'],
+    url='https://github.com/wpk-/aapi_versioned',
+    license='MIT',
+    author='Paul Koppen',
+    author_email='github@paulkoppen.com',
+    description='Een database kopie van de Amsterdam API, met versioning.'
+)
